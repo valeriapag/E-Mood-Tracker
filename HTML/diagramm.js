6950a7401364eeb74d322f3d564362c4c310e4cd
Vale <valeria.pagliaro@student.reutlingen-university.de>
2019-06-06 12:20:29 +0200

    diagramm.js

--- HTML/diagramm.js
+++ HTML/diagramm.js
@@ -6,30 +6,31 @@ const CHART= document.getElementById("lineChart");
 console.log(CHART);
 let lineChart= new Chart(CHART, {
     type: 'line',
-    data: data={
-        labels: ["Januar", "Februar", "März", "April", "Mai", "Juni","Juli"],
-        datasets:[
+    data: {
+        labels: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli"],
+        datasets: [
             {
                 label: "My first dataset",
-                fill:false,
+                fill: false,
                 lineTension: 0.1,
-                backdropColor:"rgba(75,192,192,0.4)",
+                backdropColor: "rgba(75,192,192,0.4)",
                 borderColor: "rgba(75,192,192,1)",
                 borderCapStyle: 'butt',
                 borderDash: [],
                 borderDashOffset: 0.0,
-                borderJoinStyle:'mitter',
+                borderJoinStyle: 'mitter',
                 pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor:"#fff",
+                pointBackgroundColor: "#fff",
                 pointBorderWidth: 1,
                 pointHoverRadius: 5,
-                pointHoverBackgroundColor:"rgba(75,192,192,1)",
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
                 pointHoverBorderColor: "rgba(220,220,220,1)",
-                pointHoverBorderWidth:2,
-                pointRadius:1,
-                pointHitRadius:10,
-                data:[65,59,80,81,56,55,40],
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
             }
         ]
     }
+
 });
