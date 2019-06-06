c921894b36e14f059d65a236d442e7fa1e4626ad
j.gutnik <Jan.Gutnik@student.reutlingen-university.de>
2019-06-06 14:20:10 +0200

    Changed Ausloggen.js #2

--- HTML/Ausloggen.js
+++ HTML/Ausloggen.js
@@ -3,8 +3,34 @@
     Function to delete cookie and alert to server
  */
 
-function jsLogout () {
-
+function jsLogout (url) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            swal.fire({
+                toast: true,
+                position: 'center',
+                showConfirmButton: false,
+                timer: 2000,
+                type: 'success',
+                title: 'Erfolgreich ausgeloggt!'
+            });
+        };
+    };
+    xhr.open("GET", url,true);
+    //  Set timeout duration
+    xhr.timeout = 5000;
+    //  Define function on timeout -> Show popup "Keine Verbindung"
+    xhr.ontimeout = function () {
+        Swal.fire({
+            title: 'Keine Verbindung!',
+            type: 'error',
+            backdrop: 'true',
+            confirmButtonText: 'Ok'
+        });
+        $("#load").removeAttr('disabled').html('Login');
+    };
+    xhr.send(data);
 }
 
 $(function () {
@@ -17,7 +43,7 @@ $(function () {
         $this.attr('disabled', 'disabled').html("<span " +
             "class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span>");
         //  Send logout info to server
-        //  server url
+        //  server url /logout
         var url = "https://httpbin.org/get";
         jsLogout(url);
     });
