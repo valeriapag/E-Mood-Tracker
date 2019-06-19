4394751f45b8bd33f46e90493e1c0d5fca9ce636
Shabithan <50236829+Shabithan@users.noreply.github.com>
2019-06-19 14:06:01 +0200

    Add files via upload

--- DB.js
+++ DB.js
@@ -36,13 +36,13 @@ MongoClient.connect(url, function(err, db){  //Datenbankverbindug zur URL erstel
 */
 
 var mysql = require('mysql');
-/*
-var con1 = mysql.createConnection({
+
+var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root"
 });
-*/
+
 var con1 = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -57,7 +57,7 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Database created");
   });
-  var sql = "CREATE TABLE Patient (PatientenID VARCHAR(25), Vorname VARCHAR(25), Nachname VARCHAR(25), Geburtsdatum Date, Geburtsort VARCHAR(25), Adresse VARCHAR(255), Email VARCHAR(25), PsychologenID VARCHAR(25))";
+  var sql = "CREATE TABLE Patient (PatientenID VARCHAR(25), Vorname VARCHAR(25), Nachname VARCHAR(25), Geburtsdatum Date, Geburtsort VARCHAR(25), Strasse VARCHAR(25), Hausnummer Integer, PLZ VARCHAR(10), Ort VARCHAR(25), Email VARCHAR(50),Geschlecht VARCHAR(25), Krankheit VARCHAR(25), PsychologenID VARCHAR(25))";
   con1.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Patient Table created");
@@ -72,7 +72,7 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Tagebuch Table created");
   });
-  var sql3 = "CREATE TABLE Psychologe (PsychologenID VARCHAR(25), Vorname VARCHAR(25), Nachname VARCHAR(25), Geburtsdatum Date, Geburtsort VARCHAR(25), Adresse VARCHAR(255), Email VARCHAR(25))";
+  var sql3 = "CREATE TABLE Psychologe (PsychologenID VARCHAR(25), Vorname VARCHAR(25), Nachname VARCHAR(25), Geburtsdatum Date, Geburtsort VARCHAR(25), Strasse VARCHAR(25), Hausnummer Integer, PLZ VARCHAR(10), Ort VARCHAR(25), Email VARCHAR(50))";
   con1.query(sql3, function (err, result) {
     if (err) throw err;
     console.log("Psychologe Table created");
@@ -82,4 +82,29 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Psychologenlogin Table created");
   });
+  
+  var sql = "INSERT INTO Patient (PatientenID, Vorname , Nachname,Geburtsdatum, Geburtsort, Strasse, Hausnummer, PLZ, Ort, Email,Geschlecht, Krankheit, PsychologenID) VALUES ('200', 'Hans','Peter', 15/09/1996 , 'Reutlingen', 'Alteburgstrasse', 16, '73765', 'Reutlingen', 'Hans.peter@hotmail.de', 'Männlich', 'Depression',100)";
+  con1.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Patient inserted");
+  });
+  
+  sql = "INSERT INTO Patientenlogin (PatientenID, Username , Passworthash) VALUES ('200', 'Peter', 'Hans')";
+  con1.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Patientenlogin inserted");
+  });
+  
+  sql = "INSERT INTO Psychologe (PsychologenID, Vorname , Nachname,Geburtsdatum, Geburtsort, Strasse, Hausnummer, PLZ, Ort, Email ) VALUES ('100', 'Dieter','Bohlen', 11/08/1963 , 'Metzingen', 'outletstrasse', 55, '73760', 'Metzingen', 'Dieter.Bohlen@KHtuebingen.de')";
+  con1.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Psychologe inserted");
+  });
+  
+  sql = "INSERT INTO Psychologenlogin (PsychologenID, Username , Passworthash) VALUES ('100', 'Bohlen', 'Dieter')";
+  con1.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Pschologenlogin inserted");
+  });
+  con.end();
 });
